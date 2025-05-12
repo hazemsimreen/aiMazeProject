b609762ca17ab1bf76e4e63c2921b19d8d3e517d
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-05-12 21:20:28 +0300

    Changed size for grid (accomidation)

--- src/main/java/org/example/aiproject/InputGridController.java
+++ src/main/java/org/example/aiproject/InputGridController.java
@@ -38,8 +38,8 @@ public class InputGridController {
             gridController.setRowsCount(rows);
             gridController.setColumnsCount(columns);
 
-            // Show the new scene with size 700x500
-            Scene scene = new Scene(root, 500, 700);
+            // Show the new scene with size 500x600
+            Scene scene = new Scene(root, 560, 660);
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(scene);
             stage.show();
